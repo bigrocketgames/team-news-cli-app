83fb684c52326a5f47bafe4ed9cdfb7de3a8fb20
NATHAN ROBBERT <admin@bigrocketgames.com>
2016-11-24 17:00:20 +0000

    added self.all and initialize methods to Story class.

--- lib/team_news/story.rb
+++ lib/team_news/story.rb
@@ -1,11 +1,24 @@
 class TeamNews::Story
-  attr_accessor :headline, :publisher, :date, :summary, :link, :sub, :author
-  @@teamScraper = TeamNews::Scraper.new
-  stories = []
+  attr_accessor :headline, :publisher, :date, :summary, :link, :subscription, :author
+  @@all = []
+
+  def initialize(headline, publisher, date, summary, link, author, subscription)
+    @headline = headline
+    @publisher = publisher
+    @date = date
+    @summary = summary
+    @link = link
+    @author = author
+    @subscription = subscription
+  end
 
   def self.story_list(url)
-    doc = @@teamScraper.team_scrape(url)
-    slide_count = @@teamScraper.number_of_slides(doc)
+    doc = TeamNews::Scraper.new.team_scrape(url)
+    slide_count = TeamNews::Scraper.new.number_of_slides(doc)
+  end
+
+  def self.all
+    @@all
   end
 
 end
