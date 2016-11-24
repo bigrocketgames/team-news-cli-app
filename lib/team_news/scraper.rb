04a871d6f86eab45ede9bfd1eca98c827a73bda4
NATHAN ROBBERT <admin@bigrocketgames.com>
2016-11-24 23:38:03 +0000

    Figured out the css selectors for the headline, date, and publisher.  Also, found way to determine ad versus story.

--- lib/team_news/scraper.rb
+++ lib/team_news/scraper.rb
@@ -18,9 +18,25 @@ class TeamNews::Scraper
   def scrape_for_stories(doc)
     slide_count = number_of_slides(doc)
     count = 0
-    while count < slide_count
-      puts doc.search("li[data-index='#{count.to_s}'] a div.content h3[itemprop='headline'] span:last-child").text.strip
-      binding.pry
+    # while count < slide_count
+    #   puts doc.search("li[data-index='#{count.to_s}'] a div.content h3[itemprop='headline'] span:last-child").text.strip
+    #   binding.pry
+    #   count += 1
+    # end
+
+    doc.css("div.home-wrap div.homestream div.base-note").each do |i|
+      if count < 3
+        puts count
+        test = i.css("div.story-info div.under-title span.date").text.strip
+        if test == ""
+          puts "This is an ad"
+        else
+          puts test
+        end
+        # puts i.css("div.story-info div.under-title span.date").text.strip
+        # puts i.css("div.story-info div.under-title div.site-name").text.strip
+        # puts i.css("div.story-info h4.title span.title-text span").text.strip
+      end
       count += 1
     end
 
