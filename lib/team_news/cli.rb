8c16d11ba2f8885e93d7a49697f07e18f80dfdd6
NATHAN ROBBERT <admin@bigrocketgames.com>
2016-11-27 19:27:57 +0000

    ncaa conferences now list.  SEC teams will list as well.  Need to finish switch case for conference choice.

--- lib/team_news/cli.rb
+++ lib/team_news/cli.rb
@@ -3,6 +3,9 @@ require 'pry'
 class TeamNews::CLI
   SPORTS = ["NFL", "NCAA"]
   NFL_TEAMS = [["Arizona Cardinals", "https://www.scout.com/nfl/cardinals"], ["Atlanta Falcons", "https://www.scout.com/nfl/falcons"], ["Baltimore Ravens", "https://www.scout.com/nfl/ravens"], ["Buffalo Bills", "https://www.scout.com/nfl/bills"], ["Caronlina Panthers", "https://www.scout.com/nfl/panthers"], ["Chicago Bears", "https://www.scout.com/nfl/bears"], ["Cincinnati Bengals", "https://www.scout.com/nfl/bengals"], ["Cleveland Browns", "https://www.scout.com/nfl/browns"], ["Dallas Cowboys", "https://www.scout.com/nfl/cowboys"], ["Denver Broncos", "https://www.scout.com/nfl/broncos"], ["Detroit Lions", "https://www.scout.com/nfl/lions"], ["Green Bay Packers", "https://www.scout.com/nfl/packers"], ["Houston Texans", "https://www.scout.com/nfl/texans"], ["Indianapolis Colts", "https://www.scout.com/nfl/colts"], ["Jacksonville Jaguars", "https://www.scout.com/nfl/jaguars"], ["Kansas City Chiefs", "https://www.scout.com/nfl/chiefs"], ["Los Angeles Rams", "https://www.scout.com/nfl/rams"], ["Miami Dolphins", "https://www.scout.com/nfl/dolphins"], ["Minnesota Vikings", "https://www.scout.com/nfl/vikings"], ["New England Patriots", "https://www.scout.com/nfl/patriots"], ["New Orleans Saints", "https://www.scout.com/nfl/saints"], ["New York Giants", "https://www.scout.com/nfl/giants"], ["New York Jets", "https://www.scout.com/nfl/jets"], ["Oakland Raiders", "https://www.scout.com/nfl/raiders"], ["Philadelphia Eagles", "https://www.scout.com/nfl/eagles"], ["Pittsburgh Steelers", "https://www.scout.com/nfl/steelers"], ["Sand Diego Chargers", "https://www.scout.com/nfl/chargers"], ["San Francisco 49ers", "https://www.scout.com/nfl/49ers"], ["Seattle Seahawks", "https://www.scout.com/nfl/seahawks"], ["Tampa Bay Buccaneers", "https://www.scout.com/nfl/buccaneers"], ["Tennessee Titans", "https://www.scout.com/nfl/titans"], ["Washington Redskins", "https://www.scout.com/nfl/redskins"]]
+  NCAA_CONFERENCES = ["SEC", "ACC", "BIG 10", "BIG 12", "AMERICAN" "PAC 12", "CONFERENCE USA", "MOUNTAIN WEST", "MAC", "SUN BELT", "INDEPENDENTS"]
+  SEC_TEAMS = [["Alabama Crimson Tide", "https://www.scout.com/college/alabama"], ["Arkansas Razorbacks", "https://www.scout.com/college/arkansas"], ["Auburn Tigers", "https://www.scout.com/college/auburn"], ["Florida Gators", "https://www.scout.com/college/florida"], ["Georgia Bulldogs", "https://www.scout.com/college/georgia"], ["Kentucky Wildcats", "https://www.scout.com/college/kentucky"], ["LSU Tigers", "https://www.scout.com/college/lsu"], ["Mississippi Rebels", "https://www.scout.com/college/ole-miss"], ["Mississippi State Bulldogs", "https://www.scout.com/college/mississippi-state"], ["Missouri Tigers", "https://www.scout.com/college/missouri"], ["South Carolina Gamecocks", "https://www.scout.com/college/south-carolina"], ["Tennessee Volunteers", "https://www.scout.com/college/tennessee"], ["Texas A&M Aggies", "https://www.scout.com/college/texas-am"], ["Vanderbilt Commodores", "https://www.scout.com/college/vanderbilt"]]
+  NCAA_ACC_TEAMS = []
 
   def call
     puts "Welcome to Team News"
@@ -12,12 +15,10 @@ class TeamNews::CLI
 
   def list_sports
     count = 1
-
     SPORTS.each do |s|
       puts "#{count}. #{s}"
       count += 1
     end
-
     choice = nil
 
     while choice != "exit" && choice != "1"
@@ -31,7 +32,7 @@ class TeamNews::CLI
         list_nfl_teams
       elsif choice == "2"
         puts " "
-        list_ncaa_teams
+        list_ncaa_conferences
       elsif choice == "exit"
         puts "Goodbye"
       end
@@ -45,10 +46,8 @@ class TeamNews::CLI
       puts "#{count}. #{t[0]}"
       count += 1
     end
-
     choice = nil
 
-    # turn this to a loop to make sure only proper answers are entered
     while choice != "0" && choice != "exit" && !choice.to_i.between?(1,32)
       puts " "
       puts "Please enter the number of the team you would like to see headlines for."
@@ -108,6 +107,42 @@ class TeamNews::CLI
           end
         end
       end
+    end
+  end
+
+  def list_ncaa_conferences
+    count = 1
+    NCAA_CONFERENCES.each do |c|
+      puts "#{count}. #{c}"
+      count += 1
+    end
+    choice = nil
+
+    while choice != "0" && choice != "exit" && !choice.to_i.between?(1,11)
+      puts " "
+      puts "Please enter the number of the conference you would like to see the team for."
+      puts "Enter 0 to go back to the main menu or 'exit' to end your session."
+      choice = gets.strip.downcase
+      if choice == "0"
+        puts " "
+        call
+      elsif choice == "exit"
+        puts "Goodbye"
+      elsif choice.to_i.between?(1,11)
+        puts " "
+        case choice.to_i
+        when 1
+          list_ncaa_teams(SEC_TEAMS)
+        end
+      end
+    end
+  end
+
+  def list_ncaa_teams(conference)
+    count = 1
+    conference.each do |t|
+      puts "#{count}. #{t[0]}"
+      count += 1
     end
   end
 
